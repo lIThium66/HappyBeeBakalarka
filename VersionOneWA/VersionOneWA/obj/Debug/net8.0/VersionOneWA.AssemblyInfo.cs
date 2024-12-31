5efe476334c0b768f990e38f0fb5640eee3e241e
lIThium66 <bajza357@gmail.com>
2024-12-31 19:00:54 +0100

    Uprava Beehive okna + .css pre editbeehive
    Pridane vlastnosti beehive a upravene css

--- VersionOneWA/VersionOneWA/obj/Debug/net8.0/VersionOneWA.AssemblyInfo.cs
+++ VersionOneWA/VersionOneWA/obj/Debug/net8.0/VersionOneWA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c0de7761a5589b514479882cdfe22fcabe104a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fb7a200837644c3857193fd4d37aba57f52b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
