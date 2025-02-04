e9a73b397ed287186b9633ea22b58d07f9d20077
lIThium66 <bajza357@gmail.com>
2025-02-04 19:54:39 +0100

    Icon change
    Changed icons and some design

--- VersionOneWA/VersionOneWA/obj/Debug/net8.0/VersionOneWA.AssemblyInfo.cs
+++ VersionOneWA/VersionOneWA/obj/Debug/net8.0/VersionOneWA.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b464c43f26c46cd04c710a04e84aae5e6d2dc36e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8226e07b980f9de6a734740a0bf65f40c89b9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VersionOneWA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
